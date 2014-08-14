2e7d79c840a6bba258bdc60d0d5b58d76158477f
Emmanuel NALEPA <enalepa@aldebaran-robotics.com>
2014-08-14 10:15:01 +0200

    First commit

---
+++ conftest.py
@@ -0,0 +1,156 @@
+from naoqi import ALProxy
+import tools
+import time
+import subdevice
+import pytest
+
+def pytest_addoption(parser):
+    """Configuration of pytest parsing."""
+    parser.addoption(
+        "--ip",
+        action="store",
+        default="127.0.0.1",
+        help="Robot IP"
+        )
+    parser.addoption(
+        "--port",
+        action="store",
+        default=9559,
+        help="Robot port"
+        )
+
+@pytest.fixture(scope="session")
+def robot_ip(request):
+    """Robot IP adress."""
+    return request.config.getoption("--ip")
+
+@pytest.fixture(scope="session")
+def port(request):
+    """Returns port."""
+    return request.config.getoption("--port")
+
+@pytest.fixture(scope="session")
+def mem(robot_ip, port):
+    """Fixture which returns a proxy to ALMemory module."""
+    return ALProxy("ALMemory", robot_ip, port)
+
+@pytest.fixture(scope="session")
+def motion(robot_ip, port):
+    """Try to make a proxy to ALMotion."""
+    try:
+        return ALProxy("ALMotion", robot_ip, port)
+    except RuntimeError:
+        return "MotionKilled"
+
+@pytest.fixture(scope="session", autouse=True)
+def kill(motion):
+    """
+    Fixture which kills ALMotion module.
+    It allows to use DCM module without conflict.
+    """
+    try:
+        motion.exit()
+    except:
+        pass
+
+@pytest.fixture(scope="session")
+def dcm(robot_ip, port):
+    """Proxy to DCM module."""
+    return ALProxy("DCM", robot_ip, port)
+
+@pytest.fixture(scope="session")
+def system(robot_ip, port):
+    """Proxy to ALSystem module."""
+    return ALProxy("ALSystem", robot_ip, port)
+
+@pytest.fixture(scope="session")
+def robot(system):
+    """Fixture which gives robot name."""
+    return system.robotName()
+
+@pytest.fixture(scope="session")
+def wake_up_pos():
+    """
+    Fixture which retrieves wakeUp joints position from a configuration file.
+    """
+    return tools.read_section("../global_test_configuration/juliette_positions.cfg", "wakeUp")
+
+@pytest.fixture(scope="session")
+def rest_pos():
+    """
+    Fixture which retrieves rest joints position from a configuration file.
+    """
+    return tools.read_section("../global_test_configuration/juliette_positions.cfg", "rest")
+
+@pytest.fixture(scope="session")
+def zero_pos():
+    """
+    Fixture which retrieves zero joints position from a configuration file.
+    """
+    return tools.read_section("../global_test_configuration/juliette_positions.cfg", "zero")
+
+@pytest.fixture(scope="session", autouse=True)
+def stiff_robot(request, dcm, mem, rest_pos):
+    """
+    This method automatically stiffs the robot at the beginning of the
+    tests. Once all the tests have been done, the robot goes to rest position
+    and stiffness is set to 0.
+    """
+    list_hardness = mem.getDataList("Hardness/Actuator/Value")
+    dcm.createAlias(["Hardness", list_hardness])
+    dcm.set(["Hardness", "Merge", [[1.0, dcm.getTime(0)]]])
+    def fin():
+        """Method automatically executed at the end of the test."""
+        subdevice.multiple_set(dcm, mem, rest_pos, wait=True)
+        dcm.set(["Hardness", "Merge", [[0.1, dcm.getTime(0)]]])
+        dcm.set(["Hardness", "Merge", [[0., dcm.getTime(1000)]]])
+
+    request.addfinalizer(fin)
+
+@pytest.fixture(scope="session", autouse=True)
+def stiff_robot_wheels(request, dcm, mem):
+    """
+    This method automatically stiffs the robot wheels at the beginning of the
+    tests. Once all the tests have been done, the robot goes to rest position
+    and stiffness is set to 0.
+    """
+    list_hardness = mem.getDataList("Stiffness/Actuator/Value")
+    dcm.createAlias(["WheelsHardness", list_hardness])
+    dcm.set(["WheelsHardness", "Merge", [[1.0, dcm.getTime(0)]]])
+    def fin():
+        """Method automatically executed at the end of the test."""
+        dcm.set(["WheelsHardness", "Merge", [[0.1, dcm.getTime(0)]]])
+        dcm.set(["WheelsHardness", "Merge", [[0., dcm.getTime(1000)]]])
+
+    request.addfinalizer(fin)
+
+@pytest.fixture(scope="session")
+def result_base_folder(system, mem):
+    """
+    Fixture which returns the result base folder, where we want to
+    store the log files.
+    """
+    local_time = time.localtime(time.time())
+    year = tools.int_to_two_digit_string(local_time[0])
+    month = tools.int_to_two_digit_string(local_time[1])
+    day = tools.int_to_two_digit_string(local_time[2])
+    hour = tools.int_to_two_digit_string(local_time[3])
+    minu = tools.int_to_two_digit_string(local_time[4])
+    sec = tools.int_to_two_digit_string(local_time[5])
+
+    result_folder_name = "{HeadType}_{HeadID}_{SystemVersion}_{Year}_{Month}_{Day}_{Hour}:{Min}:{Sec}".format(
+        HeadType=mem.getData("RobotConfig/Head/Type"),
+        HeadID=mem.getData("RobotConfig/Head/HeadId"),
+        SystemVersion=system.systemVersion(),
+        Year=year,
+        Month=month,
+        Day=day,
+        Hour=hour,
+        Min=minu,
+        Sec=sec
+        )
+
+    return tools.read_parameter(
+        "../global_test_configuration/parameters.cfg",
+        "General",
+        "ResultsFolder") + "/" + result_folder_name
---
+++ test_0_joint_enslavement/conftest.py
@@ -0,0 +1,122 @@
+import pytest
+import subdevice
+import tools
+
+
+@pytest.fixture(params=tools.use_section("joint_enslavement.cfg", "JulietteJoints"))
+def test_objects_dico(request, result_base_folder, dcm, mem):
+    """
+    Create the appropriate objects for each joint.
+    It logs the informations into a dictionnary and save the data into a file
+    after each joint test.
+    """
+    joint_position_actuator = subdevice.JointPositionActuator(
+        dcm, mem, request.param)
+    joint_position_sensor = subdevice.JointPositionSensor(
+        dcm, mem, request.param)
+    logger = tools.Logger()
+    def fin():
+        """Method executed after a joint test."""
+        result_file_path = "/".join(
+            [
+                result_base_folder,
+                joint_position_actuator.subdevice_type,
+                joint_position_actuator.short_name
+                ]) + ".csv"
+        logger.log_file_write(result_file_path)
+
+    request.addfinalizer(fin)
+    # creating a dictionnary with all the objects
+    dico_object = {
+        "jointActuator":joint_position_actuator,
+        "jointSensor":joint_position_sensor,
+        "logger":logger
+        }
+    return dico_object
+
+@pytest.fixture(scope="module")
+def test_leg_dico(request, result_base_folder, dcm, mem):
+    """
+    Create the appropriate objects for each joint.
+    It logs the informations into a dictionnary and save the data into a file
+    after each joint test.
+    """
+    hippitch_position_actuator = subdevice.JointPositionActuator(
+        dcm, mem, "HipPitch")
+    hippitch_position_sensor = subdevice.JointPositionSensor(
+        dcm, mem, "HipPitch")
+    kneepitch_position_actuator = subdevice.JointPositionActuator(
+        dcm, mem, "KneePitch")
+    kneepitch_position_sensor = subdevice.JointPositionSensor(
+        dcm, mem, "KneePitch")
+    logger = tools.Logger()
+    def fin():
+        """Method executed after a joint test."""
+        result_file_path = "/".join(
+            [
+                result_base_folder,
+                hippitch_position_actuator.subdevice_type,
+                "Leg"
+                ]) + ".csv"
+        logger.log_file_write(result_file_path)
+
+    request.addfinalizer(fin)
+    # creating a dictionnary with all the objects
+    dico_object = {
+        "hipActuator":hippitch_position_actuator,
+        "hipSensor":hippitch_position_sensor,
+        "kneeActuator":kneepitch_position_actuator,
+        "kneeSensor":kneepitch_position_sensor,
+        "logger":logger
+        }
+    return dico_object
+
+@pytest.fixture(params=tools.use_section("joint_enslavement.cfg", "JulietteWheels"))
+def test_wheels_dico(request, result_base_folder, dcm, mem):
+    """
+    Create the appropriate objects for each joint.
+    It logs the informations into a dictionnary and save the data into a file
+    after each wheel test.
+    """
+    wheel_speed_actuator = subdevice.WheelSpeedActuator(
+        dcm, mem, request.param)
+    wheel_speed_sensor = subdevice.WheelSpeedSensor(
+        dcm, mem, request.param)
+    logger = tools.Logger()
+    def fin():
+        """Method executed after a joint test."""
+        result_file_path = "/".join(
+            [
+                result_base_folder,
+                wheel_speed_actuator.subdevice_type,
+                wheel_speed_actuator.short_name
+                ]) + ".csv"
+        logger.log_file_write(result_file_path)
+
+    request.addfinalizer(fin)
+    # creating a dictionnary with all the objects
+    dico_object = {
+        "wheelActuator":wheel_speed_actuator,
+        "wheelSensor":wheel_speed_sensor,
+        "logger":logger
+        }
+    return dico_object
+
+@pytest.fixture(scope="module")
+def test_time():
+    """It returns the test time in milliseconds [ms]"""
+    return int(tools.read_parameter(
+        "joint_enslavement.cfg", "Parameters", "TestTime"))
+
+@pytest.fixture(scope="module")
+def test_limit():
+    """It returns the limit error in degrees."""
+    return float(tools.read_parameter(
+        "joint_enslavement.cfg", "Parameters", "TestLimit"))
+
+@pytest.fixture(scope="module")
+def test_wheels_limit():
+    """It returns the limit error in radians per seconds."""
+    return float(tools.read_parameter(
+        "joint_enslavement.cfg", "Parameters", "TestWheelsLimit"))
+
