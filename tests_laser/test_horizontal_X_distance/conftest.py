1234f6e456fc75fee274e1a29014f4f05ab33c4b
Antoine Martin <amartin@ALD-1166-DE.(none)>
2014-08-25 16:40:55 +0200

    creation of conftest.py
    Change-Id: I702e5062ed8bb8e8307573a3af1cba0bfc105258
    Reviewed-on: http://gerrit.aldebaran.lan/44804
    Approved: enalepa <enalepa@aldebaran-robotics.com>
    Reviewed-by: enalepa <enalepa@aldebaran-robotics.com>
    Tested-by: enalepa <enalepa@aldebaran-robotics.com>

---
+++ tests_laser/test_horizontal_X_distance/conftest.py
@@ -0,0 +1,60 @@
+import pytest
+from subdevice import Laser
+import tools
+import ConfigParser
+
+
+@pytest.fixture(scope="session")
+def config_test():
+    """
+    Reading test configuration file
+    """
+    cfg = ConfigParser.ConfigParser()
+    cfg.read('TestConfig.cfg')
+    return cfg
+
+@pytest.fixture(params=tools.use_section("TestConfig.cfg", "Horizontal_Side"))
+def side(request):
+    """
+    Fixture which return the side to be tested
+    """
+    return request.param
+
+@pytest.fixture(scope="module")
+def get_horizontal_x_segments(request, result_base_folder, dcm, mem, side):
+    """
+    Put the X coordinates of the horizontal laser in a dictionary
+    """
+    dico = {}
+    for i in range(1, 10):
+        dico["seg" + str(i)] = Laser(dcm, mem, side + "/Horizontal/Seg0"
+                                     + str(i) + "/X/Sensor")
+    for i in range(10, 16):
+        dico["seg" + str(i)] = Laser(dcm, mem, side + "/Horizontal/Seg"
+                                     + str(i) + "/X/Sensor")
+    logger = tools.Logger()
+    dico["logger"] = logger
+
+    def fin():
+        """Method executed after a joint test."""
+        result_file_path = "/".join(
+            [
+                result_base_folder,
+                side
+            ]) + ".csv"
+        logger.log_file_write(result_file_path)
+
+    request.addfinalizer(fin)
+    return dico
+
+@pytest.fixture(scope="session")
+def wakeup(request, motion):
+    """
+    Make the robot wakeUp at the beginning
+    of the test and go to rest at the end
+    """
+    motion.wakeUp()
+    def fin():
+        """Method automatically executed at the end of the test"""
+        motion.rest()
+    request.addfinalizer(fin)
