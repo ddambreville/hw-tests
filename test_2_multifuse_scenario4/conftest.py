5b71aa63b37db6d1299c00c064a3713c3f74c393
Thomas CHARLES <tcharles@presta.aldebaran-robotics.fr>
2014-09-02 11:58:16 +0200

    Update of multiFuseBoard test.
    Change-Id: Ib2dda053619d6b87e52ab2ddf5efebe5774dd34f
    Reviewed-on: http://gerrit.aldebaran.lan/45138
    Approved: enalepa <enalepa@aldebaran-robotics.com>
    Reviewed-by: enalepa <enalepa@aldebaran-robotics.com>
    Tested-by: enalepa <enalepa@aldebaran-robotics.com>

--- test_2_multifuse_scenario4/conftest.py
+++ test_2_multifuse_scenario4/conftest.py
@@ -2,25 +2,22 @@ import pytest
 import subdevice
 import tools
 
-@pytest.fixture(
-    params=tools.use_section("Configuration/multiFuseBoard.cfg", "Test"))
+@pytest.fixture(params=tools.use_section("multifuse_scenario4.cfg", "Test"))
 def fuse_temperature(request, dcm, mem):
     """
     Create an object corresponding to fuse temperature sensor for each fuse.
     """
-    return subdevice.fuseTemperature(dcm, mem, request.param)
+    return subdevice.FuseTemperature(dcm, mem, request.param)
 
-@pytest.fixture(
-    params=tools.use_section("Configuration/multiFuseBoard.cfg", "Test"))
+@pytest.fixture(params=tools.use_section("multifuse_scenario4.cfg", "Test"))
 def fuse_current(request, dcm, mem):
     """Create an object corresponding to fuse current sensor for each fuse."""
-    return subdevice.fuseCurrent(dcm, mem, request.param)
+    return subdevice.FuseCurrent(dcm, mem, request.param)
 
-@pytest.fixture(
-    params=tools.use_section("Configuration/multiFuseBoard.cfg", "Test"))
+@pytest.fixture(params=tools.use_section("multifuse_scenario4.cfg", "Test"))
 def fuse_voltage(request, dcm, mem):
     """Create an object corresponding to fuse voltage sensor for each fuse."""
-    return subdevice.fuseVoltage(dcm, mem, request.param)
+    return subdevice.FuseVoltage(dcm, mem, request.param)
 
 @pytest.fixture(scope="module")
 def multi_fuseboard_ambiant_tmp(dcm, mem):
@@ -36,7 +33,7 @@ def multi_fuseboard_total_current(dcm, mem):
 def test_time():
     """Give test time."""
     return int(tools.read_parameter(
-        "Configuration/jointCurrentSensors.cfg",
+        "multifuse_scenario4.cfg",
         "Parameters",
         "TestTime"
         ))
@@ -45,7 +42,7 @@ def test_time():
 def joint_limit_extension():
     """Give joint limit extension in degrees."""
     return float(tools.read_parameter(
-        "Configuration/jointCurrentSensors.cfg",
+        "multifuse_scenario4.cfg",
         "Parameters",
         "jointLimitExtension"
         ))
--- test_2_multifuse_scenario4/test_multifuse_scenario_4.py
+++ test_2_multifuse_scenario4/test_multifuse_scenario_4.py
@@ -1,10 +1,11 @@
+import pytest
 import tools
 import subdevice
 import math
 
-@pytest.mark.usefixtures("kill_motion", "stiff_robot")
-class TestMultifuseScenario4:
-    def test_fuse_board_scenario_4(dcm, mem, rest_pos, fuse_temperature, fuse_current,
+@pytest.mark.usefixtures("kill_motion")
+class TestMultifuse:
+    def test_fuse_board_scenario_4(self, dcm, mem, fuse_temperature, fuse_current,
         fuse_voltage, multi_fuseboard_ambiant_tmp, multi_fuseboard_total_current,
         test_time, joint_limit_extension, result_base_folder):
         """
@@ -17,90 +18,106 @@ class TestMultifuseScenario4:
         """
         # flag initialization
         flag = True
+        flag_key = True
         fuse_state = True
         flag_motor_tmp = True
         # checking that ALMemory key MinMaxChange Allowed = 1
         if int(mem.getData("RobotConfig/Head/MinMaxChangeAllowed")) != 1:
             flag = False
+            flag_key = False
             print "MinMaxChangeAllowed ALMemory key missing"
-        # going to initial position
-        subdevice.multiple_set(dcm, mem, rest_pos, wait=True)
         # position dictionnary creation with normal min or max
-        state = tools.read_section(
-            "multifuse_scenario4.cfg", fuse_temperature.part)
-        print state
+        state = \
+        tools.read_section("multifuse_scenario4.cfg", fuse_temperature.part)
         # copy previous positions in order to add an increment
-        state.copy()
-        # put the joints in their original max position
-        subdevice.multiple_set(dcm, mem, state, wait=True)
+        joint_list = tools.jointlistcreation_from_state(state)
+        tools.stiff_joints(dcm, mem, joint_list)
         # defining increment in radians
         increment = math.radians(joint_limit_extension)
-        # modifying max or min position and paste in state
-        for key, value in state:
+        # modifying max or min position and put the joint in that position
+        for key, value in state.items():
             if key not in ("Time",):
                 joint = key.split("/")[0]
                 joint_object = subdevice.JointPositionActuator(dcm, mem, joint)
-                if value == "max":
+                print value[0]
+                if value[0] > 0:
                     new_maximum_angle = joint_object.maximum + increment
+                    print "passe par le max"
                 else:
                     new_maximum_angle = joint_object.minimum - increment
-            state[joint+"/Position/Actuator"] = new_maximum_angle
-        # current is going to rise in all joints
-        print state
-        subdevice.multiple_set(dcm, mem, state, wait=False)
+                    print "passe par le min"
+                joint_object.maximum = \
+                [[[new_maximum_angle, dcm.getTime(0)]], "Merge"]
+                joint_object.qvalue = (new_maximum_angle, 10000)
         # loop timer creation
         timer = tools.Timer(dcm, test_time)
         # logger creation
         logger = tools.Logger()
-        # fuse max temperature initialization
-        # fuse_max_temperature = fuse_temperature.maximum
-        joint_list = tools.jointlistcreation_from_state(state)
-        print joint_list
+        # list object creation
+        joint_hardness_list = \
+        [subdevice.JointHardnessActuator(dcm, mem, x) for x in joint_list]
+        joint_current_list = \
+        [subdevice.JointCurrentSensor(dcm, mem, joint) for joint in joint_list]
         # test loop
         while flag is True:
-            joint_state = tools.are_there_null_stiffnesses(dcm, mem, joint_list)
-            fuse_temperature_status = fuse_temperature.status
-            # If the condition is respected, we go out of the loop
-            if int(fuse_temperature_status) == 3:
-                if tools.is_stiffness_null(dcm, mem, joint_list) is True:
-                    flag = False  # we go out of the loop
-                elif tools.is_stiffness_null(dcm, mem, joint_list) is False:
-                    flag = False  # we go out of the loop
-                    fuse_state = False
-            elif int(fuse_temperature_status) < 3:
-                if joint_state[0] is True and \
-                tools.is_stiffness_null(dcm, mem, joint_list) is False:
-                    for joint in joint_state[1]:
-                        joint_temperature = \
-                        subdevice.JointTemperature(dcm, mem, joint)
-                        if joint_temperature.value < joint_temperature.maximum:
-                            flag = False  # we go out of the loop
-                            flag_motor_tmp = False
-                elif tools.is_stiffness_null(dcm, mem, joint_list) is True:
+            try:
+                fuse_temperature_status = fuse_temperature.status
+                fuse_temperature_value = fuse_temperature.value
+                listeofparams = [
+                    ("Time", timer.dcm_time() / 1000.),
+                    (multi_fuseboard_ambiant_tmp.header_name,
+                     multi_fuseboard_ambiant_tmp.value),
+                    (multi_fuseboard_total_current.header_name,
+                     multi_fuseboard_total_current.value),
+                    (fuse_temperature.header_name, fuse_temperature_value),
+                    (fuse_current.header_name, fuse_current.value),
+                    (fuse_voltage.header_name, fuse_voltage.value),
+                    ("Status", fuse_temperature_status)]
+                for joint_hardness in joint_hardness_list:
+                    new_tuple = \
+                    (joint_hardness.header_name, joint_hardness.value)
+                    listeofparams.append(new_tuple)
+                for joint_current in joint_current_list:
+                    new_tuple = (joint_current.header_name, joint_current.value)
+                    listeofparams.append(new_tuple)
+                logger.log_from_list(listeofparams)
+
+                # If the condition is respected, we go out of the loop
+                joint_state = \
+                tools.are_there_null_stiffnesses(dcm, mem, joint_list)
+                # Out of the loop if fuse temperature higher than 200 degrees
+                # To be removed once REQ_FUSE_TEMPERATURE_003 is OK
+                if fuse_temperature_value > 200:
                     flag = False
-                    for joint in joint_state[1]:
-                        joint_temperature = \
-                        subdevice.JointTemperature(dcm, mem, joint)
-                        if joint_temperature.value < joint_temperature.maximum:
-                            flag_motor_tmp = False
+                if int(fuse_temperature_status) == 3:
+                    if tools.is_stiffness_null(dcm, mem, joint_list) is True:
+                        flag = False  # we go out of the loop
+                    elif tools.is_stiffness_null(dcm, mem, joint_list) is False:
+                        flag = False  # we go out of the loop
+                        fuse_state = False
+                elif int(fuse_temperature_status) < 3:
+                    if joint_state[0] is True and \
+                    tools.is_stiffness_null(dcm, mem, joint_list) is False:
+                        for joint in joint_state[1]:
+                            joint_temperature = \
+                            subdevice.JointTemperature(dcm, mem, joint)
+                            if joint_temperature.value < joint_temperature.maximum:
+                                flag = False  # we go out of the loop
+                                flag_motor_tmp = False
+                    elif tools.is_stiffness_null(dcm, mem, joint_list) is True:
+                        flag = False
+                        for joint in joint_state[1]:
+                            joint_temperature = \
+                            subdevice.JointTemperature(dcm, mem, joint)
+                            if joint_temperature.value < joint_temperature.maximum:
+                                flag_motor_tmp = False
+            except KeyboardInterrupt:
+                flag = False  # out of test loop
 
-            logger.log(
-                ("Time", timer.dcm_time() / 1000.),
-                (multi_fuseboard_ambiant_tmp.header_name,
-                 multi_fuseboard_ambiant_tmp.value),
-                (multi_fuseboard_total_current.header_name,
-                 multi_fuseboard_total_current.value),
-                (fuse_temperature.header_name, fuse_temperature.value),
-                (fuse_current.header_name, fuse_current.value),
-                (fuse_voltage.header_name, fuse_voltage.value),
-                ("Status", fuse_temperature.status)
-                )
-
-        result_file_path = "/".join(
-            [
-                result_base_folder,
-                "_".join([str(fuse_temperature.part), str(fuse_state)])])+".csv"
+        file_name = "_".join([str(fuse_temperature.part), str(fuse_state)])
+        result_file_path = "/".join([result_base_folder, file_name])+".csv"
         logger.log_file_write(result_file_path)
 
-        assert fuse_state is True
-        assert flag_motor_tmp is True
+        assert flag_key
+        assert fuse_state
+        assert flag_motor_tmp
