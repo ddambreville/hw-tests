1eb04a07e0f53389a4dcef6e0296c7b5fa9b68d6
Antoine MARTIN <amartin@presta.aldebaran-robotics.fr>
2014-09-02 12:27:30 +0200

    Scripts Test laser dance added
    Change-Id: I7343427ee4aa0c7dd0c5d979a907a311baaae85a
    Reviewed-on: http://gerrit.aldebaran.lan/45166
    Approved: enalepa <enalepa@aldebaran-robotics.com>
    Reviewed-by: enalepa <enalepa@aldebaran-robotics.com>
    Tested-by: enalepa <enalepa@aldebaran-robotics.com>

--- tests_laser/test_faux_positifs_dance/conftest.py
+++ tests_laser/test_faux_positifs_dance/conftest.py
@@ -6,6 +6,8 @@ Created on August 22, 2014
 
 import tools
 import pytest
+from subdevice import Laser
+
 
 @pytest.fixture(params=tools.use_section("TestConfig.cfg", "Dance"))
 def dance(request):
@@ -31,3 +33,40 @@ def wakeup(request, motion):
         motion.rest()
     request.addfinalizer(fin)
 
+
+@pytest.fixture(scope="module")
+def get_lasers_x_segments(request, result_base_folder, dcm, mem):
+    """
+    Put the X coordinates of each laser in a dictionary
+    """
+    h_sides = ["Front", "Left", "Right"]
+    v_sides = ["Left", "Right"]
+    dico = {}
+    for each in h_sides:
+        for i in range(1, 10):
+            dico["Horizontal_X_seg" + str(i) + "_" + each] = Laser(
+                dcm, mem, each + "/Horizontal/Seg0" + str(i) + "/X/Sensor")
+        for i in range(10, 16):
+            dico["Horizontal_seg" + str(i) + "_" + each] = Laser(
+                dcm, mem, each + "/Horizontal/Seg" + str(i) + "/X/Sensor")
+    for each in v_sides:
+        dico["Vertical_X_seg01_" + each] = Laser(
+            dcm, mem, each + "/Vertical/Seg01/X/Sensor")
+    for i in range(1, 4):
+        dico["Shovel_X_seg" + str(i)] = Laser(
+            dcm, mem, "Front/Shovel/Seg0" + str(i) + "/X/Sensor")
+
+    logger = tools.Logger()
+    dico["logger"] = logger
+
+    def fin():
+        """Method executed after a joint test."""
+        result_file_path = "/".join(
+            [
+                result_base_folder,
+                "Dance_test"
+            ]) + ".csv"
+        logger.log_file_write(result_file_path)
+
+    request.addfinalizer(fin)
+    return dico
--- tests_laser/test_faux_positifs_dance/test_faux_positifs_dance.py
+++ tests_laser/test_faux_positifs_dance/test_faux_positifs_dance.py
@@ -7,12 +7,13 @@ Created on August 22, 2014
 import threading
 import time
 
+
 def dance_motion(dance, behavior_manager):
     """
     Dance function
     """
     apps_list = behavior_manager.getBehaviorNames()
-    #print apps_list
+    # print apps_list
     dance = "User/" + dance
     if dance in apps_list:
         print "haha"
@@ -21,16 +22,52 @@ def dance_motion(dance, behavior_manager):
             print "hihi"
             time.sleep(0.5)
 
+
 def test_faux_positifs_dance(mem, remove_diagnosis, wakeup, dance,
-    remove_safety, behavior_manager):
+                             remove_safety, behavior_manager):
+            time.sleep(0.5)
+
+
+def record_laser_data(lasers_dico, thread):
+    logger = get_horizontal_x_segments["logger"]
+    h_sides = ["Front", "Left", "Right"]
+    v_sides = ["Left", "Right"]
+    while thread.isAlive():
+        for each in h_sides:
+            for i in range(1, 10):
+                logger.log(("Horizontal_X_seg" + str(i) + "_" +
+                            each, lasers_dico["Horizontal_X_seg" + str(i) +
+                                              "_" + each].value))
+            for i in range(10, 16):
+                logger.log(
+                    ("Horizontal_X_seg" + str(i) + "_" +
+                     each, lasers_dico["Horizontal_X_seg" + str(i) +
+                                       "_" + each].value))
+        for each in v_sides:
+            logger.log(
+                ("Vertical_X_seg01_" + each, lasers_dico["Vertical_X_seg01_" + each].value))
+        for i in range(1, 4):
+            logger.log(
+                ("Shovel_X_seg" + str(i) + each, lasers_dico["Shovel_X_seg" + str(i)].value))
+
+    return logger
+
+
+def test_faux_positifs_dance(mem, remove_diagnosis, wakeup, dance,
+                             remove_safety, behavior_manager, get_lasers_x_segments):
     """
     Test function
     """
     print dance
     time.sleep(10)
     #dance_thread = threading.Thread(target=dance_motion, args=(dance, behavior_manager))
-    #dance_thread.start()
+    # dance_thread.start()
     dance_motion(dance, behavior_manager)
 
-
-
+    time.sleep(2)
+    dance_thread = threading.Thread(
+        target=dance_motion, args=(dance, behavior_manager))
+    dance_thread.start()
+    time.sleep(10)
+    logger = record_laser_data(get_lasers_x_segments, dance_thread)
+    print logger.log_dic
