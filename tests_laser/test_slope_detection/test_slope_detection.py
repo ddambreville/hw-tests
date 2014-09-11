4f36a87f112b37952614d0eb93cfd1a18cf3c87a
Antoine MARTIN <amartin@presta.aldebaran-robotics.fr>
2014-09-11 16:05:09 +0200

    Add a verification of error and nack on the laser boards at the beginning of the slop test
    Change-Id: Ic3085cfb49d38a15f3b15457460e50e8f607564d
    Reviewed-on: http://gerrit.aldebaran.lan/45655
    Approved: enalepa <enalepa@aldebaran-robotics.com>
    Reviewed-by: enalepa <enalepa@aldebaran-robotics.com>
    Tested-by: enalepa <enalepa@aldebaran-robotics.com>

---
+++ tests_laser/test_slope_detection/test_slope_detection.py
@@ -0,0 +1,36 @@
+'''
+Created on August 27, 2014
+
+@author: amartin
+
+Angle de la pente fixe - Distantes differentes
+'''
+
+import threading
+
+
+def robot_motion(motion, pos_0, coord, side, config_test):
+    """
+    Thread which make the robot move
+    """
+    # On recupere la distance a parcourir dans le fichier de config
+    distance = float(config_test.get('Test_Config', 'Distance_Parcours'))
+    while abs(motion.getRobotPosition(True)[coord] - pos_0) < distance:
+        motion.move(-0.1, 0, 0)
+
+
+def test_slope(
+    check_error_laser, dcm, mem, motion, wakeup, side,
+        get_horizontal_x_segments, config_test, remove_safety, remove_diagnosis):
+    """
+    Test function which test the X distance
+    of the horizontal lasers
+    """
+    motion_thread = threading.Thread(target=robot_motion, args=(
+        motion, pos_0, coord, side, config_test))
+    motion_thread.start()
+    logger = record_horizontaux_data(
+        get_horizontal_x_segments, motion, side, pos_0, coord, motion_thread,
+        config_test)
+    result = check_error(logger, config_test)
+    assert 'Fail' not in result
