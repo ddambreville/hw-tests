b6c74f9ba7fa448712463bc98e116376d776ef0e
Thomas CHARLES <tcharles@presta.aldebaran-robotics.fr>
2014-09-15 16:07:39 +0200

    Fan test update.
    Change-Id: I9d7dbe8fc98b9052fa469e333fa89a938ec87c34
    Reviewed-on: http://gerrit.aldebaran.lan/45802
    Approved: enalepa <enalepa@aldebaran-robotics.com>
    Reviewed-by: enalepa <enalepa@aldebaran-robotics.com>
    Tested-by: enalepa <enalepa@aldebaran-robotics.com>

--- test_5_fan/fan_utils.py
+++ test_5_fan/fan_utils.py
@@ -2,7 +2,7 @@ import subdevice
 import tools
 
 
-def fan_cycle(dcm, mem, nb_cycles, t_on=5000, t_off=5000):
+def fan_cycle(dcm, mem, nb_cycles, t_on=10000, t_off=10000):
     fan_hardness_actuator = subdevice.FanHardnessActuator(dcm, mem)
     for i in range(nb_cycles):
         fan_hardness_actuator.qqvalue = 1.0
