70744009608ba99bd44ba1ecaa8d14e07d1d360f
Antoine MARTIN <amartin@presta.aldebaran-robotics.fr>
2014-09-23 17:42:40 +0200

    Add slope detection scripts
    Change-Id: I874007a98024ada6f8b44bc58aebecda0cb523f8
    Reviewed-on: http://gerrit.aldebaran.lan/46138
    Approved: enalepa <enalepa@aldebaran-robotics.com>
    Reviewed-by: enalepa <enalepa@aldebaran-robotics.com>
    Tested-by: enalepa <enalepa@aldebaran-robotics.com>

---
+++ tests_laser/test_slope_detection/conftest.py
@@ -0,0 +1,55 @@
+#-*- coding: iso-8859-15 -*-
+
+'''
+Created on September 23, 2014
+
+@author: amartin
+'''
+
+
+import pytest
+from subdevice import Laser
+import tools
+import ConfigParser
+
+
+@pytest.fixture(scope="session")
+def config_test():
+    """
+    Reading test configuration file
+    """
+    cfg = ConfigParser.ConfigParser()
+    cfg.read('TestConfig.cfg')
+    return cfg
+
+
+@pytest.fixture(scope="module")
+def get_slope_segments(request, result_base_folder, dcm, mem):
+    """
+    Return a dictionary with several objects for
+    each X coordinate of all horizontal segments
+    """
+    dico = {}
+    dico["Left_Inclinaison"] = Laser(
+        dcm, mem, "Front/Vertical/Left/Slope/Inclination/Sensor")
+    dico["Left_Distance"] = Laser(
+        dcm, mem, "Front/Vertical/Left/Slope/X/Sensor")
+    dico["Right_Inclinaison"] = Laser(
+        dcm, mem, "Front/Vertical/Right/Slope/Inclination/Sensor")
+    dico["Right_Distance"] = Laser(
+        dcm, mem, "Front/Vertical/Right/Slope/X/Sensor")
+
+    logger = tools.Logger()
+    dico["logger"] = logger
+
+    def fin():
+        """Method executed after a joint test."""
+        result_file_path = "/".join(
+            [
+                result_base_folder,
+                "Vertical_Test"
+            ]) + ".csv"
+        logger.log_file_write(result_file_path)
+
+    request.addfinalizer(fin)
+    return dico
--- tests_laser/test_slope_detection/test_slope_detection.py
+++ tests_laser/test_slope_detection/test_slope_detection.py
@@ -7,30 +7,113 @@ Angle de la pente fixe - Distantes differentes
 '''
 
 import threading
+from termcolor import colored
+import time
 
 
-def robot_motion(motion, pos_0, coord, side, config_test):
+def robot_motion(motion, pos_0, config_test):
     """
     Thread which make the robot move
     """
     # On recupere la distance a parcourir dans le fichier de config
-    distance = float(config_test.get('Test_Config', 'Distance_Parcours'))
-    while abs(motion.getRobotPosition(True)[coord] - pos_0) < distance:
+    distance = float(config_test.get('Test_Config', 'Distance_travel'))
+    while abs(motion.getRobotPosition(True)[0] - pos_0) < distance:
         motion.move(-0.1, 0, 0)
 
 
+def record_slope_data(
+        get_slope_segments, motion, pos_0,
+        thread, config_test):
+    """
+    Function which logs the laser distances
+    """
+    logger = get_slope_segments["logger"]
+    angle = float(config_test.get('Test_Config', 'Angle'))
+    offset_front = float(config_test.get('Test_Config', 'Offset_Front'))
+    while thread.isAlive():
+
+        logger.log(("robot_pos", abs(
+            motion.getRobotPosition(True)[0] - pos_0) + offset_front))
+        logger.log(("Left_Inclinaison", get_slope_segments[
+            "Left_Inclinaison"].value))
+        logger.log(("Left_Distance", get_slope_segments[
+            "Left_Distance"].value))
+        logger.log(("Right_Inclinaison", get_slope_segments[
+            "Right_Inclinaison"].value))
+        logger.log(("Right_Distance", get_slope_segments[
+            "Right_Distance"].value))
+    for i in range(0, len(logger.log_dic["robot_pos"])):
+        logger.log(("Erreur_Distance_Left", (abs(
+            logger.log_dic["robot_pos"][i] - logger.log_dic[
+                "Left_Distance"][i]) / logger.log_dic["robot_pos"][i]) * 100))
+        logger.log(("Erreur_Distance_Right", (abs(
+            logger.log_dic["robot_pos"][i] - logger.log_dic[
+                "Right_Distance"][i]) / logger.log_dic["robot_pos"][i]) * 100))
+        logger.log(("Erreur_Inclinaison_Left", (abs(
+            angle - logger.log_dic["Left_Inclinaison"][i]) / angle) * 100))
+        logger.log(("Erreur_Inclinaison_Right", (abs(
+            angle - logger.log_dic["Right_Inclinaison"][i]) / angle) * 100))
+    time.sleep(0.01)
+    return logger
+
+
+def print_error(logger, name, error_vl, i):
+    """
+    Print error message
+    """
+    print name + " : " + colored("Fail", "red")
+    print "Position : " + str(logger.log_dic["robot_pos"][i])
+    print "Erreur : " + str(error_vl) + "%"
+
+
+def check_error(logger, config_test):
+    """
+    Function which checks the distance error
+    at the end of the test
+    """
+    side = ["Left", "Right"]
+    result = []
+    tolerance_a = float(
+        config_test.get('Test_Config', "Tolerance_angle"))
+    tolerance_d = float(
+        config_test.get('Test_Config', "Tolerance_distance"))
+    print len(logger.log_dic["robot_pos"])
+    for each in side:
+        for i in range(0, len(logger.log_dic["robot_pos"]) - 1):
+            error_d = logger.log_dic["Erreur_Distance_" + each][i]
+            if error_d > tolerance_d:
+                result.append('Fail')
+                print_error(logger, each + "_Distance", error_d, i)
+                break
+            elif i == len(logger.log_dic["robot_pos"]) - 2:
+                result.append('Pass')
+                print "Distance" + each + " : " + colored("Pass", "green")
+    for each in side:
+        for i in range(0, len(logger.log_dic["robot_pos"]) - 1):
+            error_a = logger.log_dic["Erreur_Inclinaison_" + each][i]
+            if error_a > tolerance_a:
+                result.append('Fail')
+                print_error(logger, each + "_Inclinaison", error_a, i)
+                break
+            elif i == len(logger.log_dic["robot_pos"]) - 2:
+                result.append('Pass')
+                print "Distance" + each + " : " + colored("Pass", "green")
+
+    return result
+
+
 def test_slope(
-    check_error_laser, dcm, mem, motion, wakeup, side,
-        get_horizontal_x_segments, config_test, remove_safety, remove_diagnosis):
+    check_error_laser, dcm, mem, motion, wakeup,
+        get_slope_segments, config_test, remove_safety, remove_diagnosis):
     """
     Test function which test the X distance
     of the horizontal lasers
     """
+    pos_0 = motion.getRobotPosition(True)[0]
     motion_thread = threading.Thread(target=robot_motion, args=(
-        motion, pos_0, coord, side, config_test))
+        motion, pos_0, config_test))
     motion_thread.start()
-    logger = record_horizontaux_data(
-        get_horizontal_x_segments, motion, side, pos_0, coord, motion_thread,
-        config_test)
+    logger = record_slope_data(
+        get_slope_segments, motion, pos_0, motion_thread, config_test)
     result = check_error(logger, config_test)
     assert 'Fail' not in result
