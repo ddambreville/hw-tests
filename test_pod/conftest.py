504360061ee52fa86e50cd02c680d57f8d60995e
candre <candre@aldebaran-robotics.com>
2014-09-25 18:46:34 +0200

    Test pod conftest simplified.
    Change-Id: Ib4c02f0b5fc9fd5dd13391d666862902da4b5ea3
    Reviewed-on: http://gerrit.aldebaran.lan/46367
    Approved: enalepa <enalepa@aldebaran-robotics.com>
    Reviewed-by: enalepa <enalepa@aldebaran-robotics.com>
    Tested-by: enalepa <enalepa@aldebaran-robotics.com>

--- test_pod/conftest.py
+++ test_pod/conftest.py
@@ -1,42 +1,11 @@
 import pytest
-
-
-def pytest_addoption(parser):
-    """Configuration of pytest parsing."""
-    parser.addoption(
-        "--testTime",
-        type=int,
-        action="store",
-        default=60,
-        help="Test time"
-    )
-    parser.addoption(
-        "--nbCycles",
-        type=int,
-        action="store",
-        default=5000,
-        help="Cycles number"
-    )
-    parser.addoption(
-        "--fileName",
-        action="store",
-        default="test_usure_pod.csv",
-        help="Output file name"
-    )
+import tools
+import subdevice
 
 
 @pytest.fixture(scope="session")
-def test_time(request):
-    """Test time"""
-    return request.config.getoption("--testTime")
-
-
-@pytest.fixture(scope="session")
-def nb_cycles(request):
-    """Cycles number"""
-    return request.config.getoption("--nbCycles")
-
-@pytest.fixture(scope="session")
-def file_name(request):
-    """Output file name"""
-    return request.config.getoption("--fileName")
+def unstiff_parts(dcm, mem):
+    joints = tools.use_section("parts.cfg", "parts")
+    for name in joints:
+        joint_hardness = subdevice.JointHardnessActuator(dcm, mem, name)
+        joint_hardness.qqvalue = 0.
