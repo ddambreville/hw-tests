3ac29d638f1ad03da86d9c5754593c0ca4e7b2f9
Antoine MARTIN <amartin@presta.aldebaran-robotics.fr>
2014-10-06 16:39:04 +0200

    [InertialSensorBase - test_static] All scripts added
    Change-Id: Ib263d6fb0f077b0c800579b303cfa3aa82619e5f
    Reviewed-on: http://gerrit.aldebaran.lan/47043
    Approved: enalepa <enalepa@aldebaran-robotics.com>
    Reviewed-by: enalepa <enalepa@aldebaran-robotics.com>
    Tested-by: enalepa <enalepa@aldebaran-robotics.com>

---
+++ test_InertialSensorBase/test_static/test_static.py
@@ -0,0 +1,28 @@
+'''
+Created on September 29, 2014
+
+@author: amartin
+
+[Description]
+This script tests the values returned by inertial base sensor
+when the robot is static.
+
+[Initial conditions]
+Robot in rest position.
+'''
+import threading
+from test_static_utils import check_error, record_inetialbase_data, wait
+
+
+
+def test_static(get_all_inertialbase_objects, config_test):
+    """
+    Test main function which tests the inertial base sensor
+    when the robot is static.
+    """
+    wait_thread = threading.Thread(target=wait, args=(config_test,))
+    wait_thread.start()
+    print "test is running ..."
+    logger = record_inetialbase_data(get_all_inertialbase_objects, wait_thread)
+    result = check_error(logger, config_test)
+    assert 'Fail' not in result
---
+++ test_InertialSensorBase/test_static/test_static_utils.py
@@ -0,0 +1,85 @@
+'''
+Created on September 29, 2014
+
+@author: amartin
+'''
+
+import time
+from termcolor import colored
+
+GRAVITY = 9.81
+
+def wait(config_test):
+    """
+    Wait function
+    """
+    time_test = float(config_test.get('Test_Config', 'Test_time'))
+    time.sleep(time_test)
+
+def print_error(sensor, value, result):
+    """
+    printe the error messages on the console
+    """
+    result.append("Fail")
+    print sensor + " : " + colored("Fail", "red")
+    print sensor + " value = " + str(value)
+    return error
+
+def error(logger, sensor, tolerance, result):
+    """
+    Docstring
+    """
+    for index, each in enumerate(logger.log_dic[sensor]):
+        if sensor == "AccZ":
+            if abs((each - GRAVITY)/GRAVITY) > tolerance:
+                result = print_error(sensor, each, result)
+                break
+            elif index == len(logger.log_dic[sensor]) - 1:
+                print sensor + " : " + colored("Pass", "green")
+        else:
+            if abs(each) > tolerance:
+                result = print_error(sensor, each, result)
+                break
+            elif index == len(logger.log_dic[sensor]) - 1:
+                print sensor + " : " + colored("Pass", "green")
+    return result
+
+def check_error(logger, config_test):
+    """
+    check error function
+    """
+    tolerance_acc_xy = float(config_test.get('Test_Config', 'Tolerance_Acc_XY'))
+    tolerance_acc_z = float(config_test.get('Test_Config', 'Tolerance_Acc_Z'))
+    tolerance_angle_xy = float(config_test.get(
+        'Test_Config', 'Tolerance_Angle_XY'))
+    tolerance_gyr_all = float(config_test.get(
+        'Test_Config', 'Tolerance_Gyr_All'))
+    result = []
+    coord = ["X", "Y"]
+    for each in coord:
+        result = error(logger, "Acc" + each, tolerance_acc_xy, result)
+        result = error(logger, "Angle" + each, tolerance_angle_xy, result)
+        result = error(logger, "Gyr" + each, tolerance_gyr_all, result)
+    result = error(logger, "AccZ", tolerance_acc_z, result)
+    result = error(logger, "GyrZ", tolerance_gyr_all, result)
+    return result
+
+def record_inetialbase_data(
+        get_all_inertialbase_objects, thread):
+    """
+    Function which logs the inertial base datas
+    """
+    logger = get_all_inertialbase_objects["logger"]
+    coord = ["X", "Y", "Z"]
+    t_0 = time.time()
+    while thread.isAlive():
+        for each in coord:
+            logger.log(("Acc" + each, get_all_inertialbase_objects[
+                "Acc" + each].value))
+            logger.log(("Angle" + each, get_all_inertialbase_objects[
+                "Angle" + each].value))
+            logger.log(("Gyr" + each, get_all_inertialbase_objects[
+                "Gyr" + each].value))
+        logger.log(("Time", time.time() - t_0))
+        time.sleep(0.005)
+    return logger
