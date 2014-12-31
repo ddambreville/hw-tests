d397fff3489aff773e684867daf38b30fa171e83
candre <candre@aldebaran-robotics.com>
2014-12-31 09:56:36 +0100

    [TOUCHDETECTION] Add script for test perf_003.
    Change-Id: I9c57516f9015a8b8da2b3e01f845eef2c84805f4
    Reviewed-on: http://gerrit.aldebaran.lan/51640
    Approved: enalepa <enalepa@aldebaran-robotics.com>
    Reviewed-by: enalepa <enalepa@aldebaran-robotics.com>
    Tested-by: enalepa <enalepa@aldebaran-robotics.com>

---
+++ test_touchdetection/test_perf_003/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+import qha_tools
+from naoqi import ALProxy
+from qi import Session
+
+
+@pytest.fixture(scope="session")
+def albehaviormanager(robot_ip, port):
+    """ Fixture which returns a proxy to ALBehaviorManager module. """
+    return ALProxy("ALBehaviorManager", robot_ip, port)
+
+
+@pytest.fixture(params=qha_tools.use_section(
+                "touchdetection_perf_003.cfg", "BehaviorsName"))
+def behaviors(request):
+    """
+    Behaviors and dances.
+    """
+    dico_behaviors = {
+        "Name": str(request.param)
+    }
+    return dico_behaviors
---
+++ test_touchdetection/test_perf_003/test_touchdetection_perf_003.py
@@ -0,0 +1,111 @@
+import pytest
+import qha_tools
+import subdevice
+import threading
+import time
+import uuid
+import plot_touchdetection
+
+
+class EventModule(object):
+
+    """
+    Module which launch function if event detected.
+    """
+
+    def __init__(self, mem):
+        """
+        @mem        : proxy to ALMemory (object)
+        """
+        self.mem = mem
+        self._flag_event = 0      # = 1 when event detected, else = 0
+        self._flag = False        # True when event detected at least one time
+
+    def subscribe(self, module_name, events):
+        """
+        Subscribe to event.
+        Run function when event is detected.
+
+        @module_name    : event module name
+        @ events        : events expected (dictionnary)
+        """
+        for k in events.keys():
+            self.mem.subscribeToEvent(k, module_name, "_event_detected")
+
+    def _event_detected(self):
+        """
+        Function triggered when event is detected.
+        Change flags.
+        """
+        event_detected = str(self.mem.getData("TouchChanged"))
+        print("Event detected : " + event_detected)
+        if "True" in event_detected:
+            self._flag_event = 1
+            self._flag = True
+        else:
+            self._flag_event = 0
+
+    def _get_flag_event(self):
+        """
+        Return flag event.
+        """
+        return self._flag_event
+
+    def _get_flag(self):
+        """
+        Return flag.
+        """
+        return self._flag
+
+    flag_event = property(_get_flag_event)
+    flag = property(_get_flag)
+
+
+def run_behavior(albehaviormanager, behavior_name):
+    """
+    Funtion to run behavior.
+    No return.
+
+    @behavior_name      : name of the behavior ro run (string)
+    @plot               : plot object (object)
+    """
+    # Verify behavior existing
+    if albehaviormanager.isBehaviorPresent(behavior_name):
+        albehaviormanager.runBehavior(behavior_name)
+    else:
+        print("\n\nBehavior " + behavior_name + "is not present")
+        print("Add the behavior and throw again")
+        assert False
+
+
+def test_touchdetection_with_dances(mem, session, albehaviormanager,
+                                motion_wake_up, behaviors):
+    """
+    Test rollonomes with dances or behaviors : no fall down.
+    Launch requested dances (cf associated config file).
+    Assert True is no fall down detected.
+
+    @mem            : proxy to ALMemory (object)
+    @session        : Session in qi (object)
+    @albehaviormanager  : proxy to ALBehavioManager (object)
+    @motion_wake_up : robot does is wakeUp
+    @behaviors          : dictionnary {"name":value} (dictionnary)
+    """
+
+    # Subcribe to module
+    expected = {"TouchChanged": 1}
+    module_name = "EventChecker_{}_".format(uuid.uuid4())
+    touchdetection = EventModule(mem)
+    module_id = session.registerService(module_name, touchdetection)
+    touchdetection.subscribe(module_name, expected)
+
+    behavior = behaviors["Name"]
+    run_behavior(albehaviormanager, behavior)
+
+    session.unregisterService(module_id)
+
+    if touchdetection.flag:
+        assert True
+
+    assert False
+
