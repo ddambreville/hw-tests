f277a7d79cc68d3927d75a5706e91227da547bcb
candre <candre@aldebaran-robotics.com>
2015-01-07 13:01:05 +0100

    [SENSORS] Add test_safety_001 : worst case test
    Change-Id: I17c2df2815b49fe4a40218fa8c6974bff8bb1f08
    Reviewed-on: http://gerrit.aldebaran.lan/51886
    Approved: enalepa <enalepa@aldebaran-robotics.com>
    Reviewed-by: enalepa <enalepa@aldebaran-robotics.com>
    Tested-by: enalepa <enalepa@aldebaran-robotics.com>

---
+++ test_sensors/test_safety_001/conftest.py
@@ -0,0 +1,51 @@
+import pytest
+import subdevice
+import qha_tools
+
+
+@pytest.fixture(scope="session")
+def parameters():
+    """
+    Return parameters (config file).
+    """
+    return qha_tools.read_section("safety_001.cfg", "TestParameters")
+
+
+@pytest.fixture(params=qha_tools.use_section("safety_001.cfg", "Speed"))
+def speed_value(request):
+    """
+    Speed
+    """
+    dico_speed = {
+        "Speed": float(request.param)
+    }
+    return dico_speed
+
+
+@pytest.fixture(params=qha_tools.use_section("safety_001.cfg", "Directions"))
+def directions(request):
+    """
+    Directions
+    """
+    dico_direction = {
+        "Direction": str(request.param)
+    }
+    return dico_direction
+
+
+@pytest.fixture(params=qha_tools.use_section(
+                "safety_001.cfg", "Dance"))
+def behaviors(request):
+    """
+    Behaviors and dances.
+    """
+    dico_behaviors = {
+        "Name": str(request.param)
+    }
+    return dico_behaviors
+
+
+@pytest.fixture(scope="session")
+def albehaviormanager(robot_ip, port):
+    """ Fixture which returns a proxy to ALBehaviorManager module. """
+    return ALProxy("ALBehaviorManager", robot_ip, port)
---
+++ test_sensors/test_safety_001/test_sensors_safety_001.py
@@ -0,0 +1,113 @@
+import pytest
+import qha_tools
+import subdevice
+import time
+import uuid
+import save_sensors_values
+import multi_logger
+
+
+class EventModule(object):
+
+    """
+    Module which launch function if event detected.
+    """
+
+    def __init__(self, mem):
+        """
+        @mem        : proxy to ALMemory (object)
+        """
+        self.mem = mem
+        self._flag_event = 0      # = 1 when event detected, else = 0
+        self._flag = False        # True when event detected at least one time
+
+    def subscribe(self, module_name, events):
+        """
+        Subscribe to event.
+        Run function when event is detected.
+
+        @module_name    : event module name
+        @ events        : events expected (dictionnary)
+        """
+        for k in events.keys():
+            self.mem.subscribeToEvent(k, module_name, "_event_detected")
+
+    def _event_detected(self):
+        """
+        Function triggered when event is detected.
+        Change flags.
+        """
+        event_detected = str(self.mem.getData("ALMotion/MoveFailed"))
+        print(str(event_detected))
+        self._flag_event = 1
+        self._flag = True
+
+    def _get_flag_event(self):
+        """
+        Return flag event.
+        """
+        return self._flag_event
+
+    def _get_flag(self):
+        """
+        Return flag.
+        """
+        return self._flag
+
+    flag_event = property(_get_flag_event)
+    flag = property(_get_flag)
+
+
+def move(motion, speed, direction):
+    motion.move(int(direction["x"][0]),
+                int(direction["y"][0]),
+                int(direction["z"][0])
+                )
+
+
+def test_sensors_safety_001(robot_ip, dcm, mem, motion, session,
+                            motion_wake_up, directions, speed_value):
+    """
+    Docstring
+    """
+    expected = {"ALMotion/MoveFailed": 1}
+    module_name = "EventChecker_{}_".format(uuid.uuid4())
+    obstacledetected = EventModule(mem)
+    module_id = session.registerService(module_name, obstacledetected)
+    obstacledetected.subscribe(module_name, expected)
+
+    # Objects creation
+    wheelb_speed_act = subdevice.WheelSpeedActuator(dcm, mem, "WheelB")
+    wheelfr_speed_act = subdevice.WheelSpeedActuator(dcm, mem, "WheelFR")
+    wheelfl_speed_act = subdevice.WheelSpeedActuator(dcm, mem, "WheelFL")
+
+    flag_test = True
+
+    speed = speed_value["Speed"]
+    direction = directions["Direction"]
+
+    log = multi_logger.Logger(
+        robot_ip,
+        "multi_logger.cfg",
+        0.1,
+        "Direction" + str(direction) + " - " + str(speed) + ".csv")
+    log.log()
+
+    # Movement
+    move(motion, speed,
+         qha_tools.read_section("safety_001.cfg", "Direction" + direction)
+         )
+
+    time.sleep(1)
+
+    while not obstacledetected.flag:
+        if wheelb_speed_act.value == 0 and \
+                wheelfr_speed_act.value == 0 and \
+                wheelfl_speed_act.value == 0:
+            flag_test = False
+
+    log.stop()
+
+    session.unregisterService(module_id)
+
+    assert flag_test
