ade93bfd486af9778c867b9cc61a99341b6b3d10
Thomas CHARLES <tcharles@presta.aldebaran-robotics.fr>
2015-01-08 15:11:34 +0100

    [Current regulation] Test update
    - Add real time plot
    - Usefull informations displayed thanks to logging module
    - Stiffness cut on non tested joints to avoid overheat
    - More commits into the script
    - Test loop conditions modified
    
    Change-Id: I9370fdc1242866f3c1473a023cbd4dbe15304256
    Reviewed-on: http://gerrit.aldebaran.lan/51962
    Approved: enalepa <enalepa@aldebaran-robotics.com>
    Reviewed-by: enalepa <enalepa@aldebaran-robotics.com>
    Tested-by: enalepa <enalepa@aldebaran-robotics.com>

--- test_3_current_limitation/test_current_limitation.py
+++ test_3_current_limitation/test_current_limitation.py
@@ -2,29 +2,47 @@ import pytest
 import qha_tools
 import subdevice
 import math
+import logging
+import easy_plot_connection
+import time
 
+logging.basicConfig(level=logging.INFO)
 
 @pytest.mark.usefixtures("kill_motion", "stiff_robot")
 class TestCurrentLimitation:
 
     def test_joint_current_limitation(self, dcm, mem, parameters,
-                                      test_objects_dico, result_base_folder,
-                                      rest_pos):
+                                      joint, result_base_folder,
+                                      rest_pos, stiffness_off,
+                                      plot, plot_server):
+        # logger initialization
+        log = logging.getLogger('test_joint_current_limitation')
+
+        # erasing real time plot
+        plot_server.curves_erase()
+
         # flags initialization
-        flag_joint = True
-        flag_max_current_exceeded = False
+        flag_loop = True  # test loop stops when this flag is False
+        flag_joint = True  # giving test result
+        flag_current_limit_low_exceeded = False
 
         # test parameters
         test_params = parameters
-        joint_position_actuator = test_objects_dico["jointPositionActuator"]
-        joint_position_sensor = test_objects_dico["jointPositionSensor"]
-        joint_temperature_sensor = test_objects_dico["jointTemperatureSensor"]
-        joint_current_sensor = test_objects_dico["jointCurrentSensor"]
+        joint_position_actuator = joint.position.actuator
+        joint_position_sensor = joint.position.sensor
+        joint_temperature_sensor = joint.temperature
+        joint_current_sensor = joint.current
         slav = qha_tools.SlidingAverage(test_params["sa_nb_points"])
         logger = qha_tools.Logger()
 
         # Going to initial position
         subdevice.multiple_set(dcm, mem, rest_pos, wait=True)
+        # unstiffing all the other joints to avoid leg motors overheat
+        subdevice.multiple_set(dcm, mem, stiffness_off, wait=False)
+        time.sleep(0.1)
+
+        # stiffing the joint we want to test
+        joint.hardness.qqvalue = 1.0
 
         # keeping initial joint min and max
         joint_initial_maximum = joint_position_actuator.maximum
@@ -41,9 +59,10 @@ class TestCurrentLimitation:
         joint_max_current = joint_current_sensor.maximum
         joint_min_current = joint_current_sensor.minimum
         delta_current = joint_max_current - joint_min_current
-        k = test_params["limit_factor"]
-        current_limit_high = joint_max_current + k * delta_current
-        current_limit_low = joint_max_current - k * delta_current
+        k_sup = test_params["limit_factor_sup"]
+        k_inf = test_params["limit_factor_inf"]
+        current_limit_high = joint_max_current + k_sup * delta_current
+        current_limit_low = joint_max_current - k_inf * delta_current
 
         # setting temperature limitatons
         joint_temperature_min = joint_temperature_sensor.minimum
@@ -67,49 +86,77 @@ class TestCurrentLimitation:
         else:
             joint_position_actuator.qvalue = (joint_new_maximum, 1000)
 
-        if joint_position_actuator.short_name in ("KneePitch", "HipPitch"):
-            timer = qha_tools.Timer(dcm, 6000)
-            timer_limit = qha_tools.Timer(dcm, 3000)
-        else:
-            timer = qha_tools.Timer(dcm, test_params["test_time"])
-            timer_limit = qha_tools.Timer(dcm, test_params["test_time_limit"])
+        timer = qha_tools.Timer(dcm, test_params["test_time"])
+        timer_limit = qha_tools.Timer(dcm, test_params["test_time_limit"])
 
-        while flag_joint is True and timer.is_time_not_out():
+        # test loop
+        while flag_loop and timer.is_time_not_out():
+            try:
+                loop_time = timer.dcm_time() / 1000.
+                joint_temperature = joint_temperature_sensor.value
+                joint_current = joint_current_sensor.value
+                slav.point_add(joint_current)
+                joint_current_sa = slav.calc()
 
-            joint_temperature = joint_temperature_sensor.value
-            joint_current = joint_current_sensor.value
-            slav.point_add(joint_current)
-            joint_current_sa = slav.calc()
+                if not flag_current_limit_low_exceeded and\
+                    joint_current_sa > current_limit_low:
+                    flag_current_limit_low_exceeded = True
+                    log.info("Current limit low exceeded")
 
-            if joint_current_sa > current_limit_low:
-                flag_current_limit_low_exceeded = True
+                if joint_current_sa > current_limit_high:
+                    flag_joint = False
+                    log.warning("Current limit high overshoot")
 
-            if joint_current_sa > current_limit_high:
-                flag_joint = False
+                if flag_current_limit_low_exceeded and\
+                    joint_current_sa < current_limit_low:
+                    flag_joint = False
+                    log.warning("Current lower than current limit low")
 
-            if flag_current_limit_low_exceeded and\
-                joint_current_sa < current_limit_low:
-                flag_joint = False
+                if timer_limit.is_time_out() and not \
+                    flag_current_limit_low_exceeded:
+                    flag_joint = False
+                    log.info("Timer limit finished and "+\
+                        "current limit low not exceeded")
 
-            if timer_limit.is_time_out() and not \
-                flag_current_limit_low_exceeded:
-                flag_joint = False
+                # out of test loop if temperature is higher than min temperature
+                if joint_temperature >= joint_temperature_min:
+                    flag_loop = False
+                    log.info("Temperature higher than Min temperature")
+                    log.info("Test finished")
 
-            if joint_temperature >= joint_temperature_min:
-                flag_joint = None
+                logger.log(
+                    ("Time", timer.dcm_time() / 1000.),
+                    ("Current", joint_current),
+                    ("CurrentSA", joint_current_sa),
+                    ("CurrentLimitHigh", current_limit_high),
+                    ("CurrentLimitLow", current_limit_low),
+                    ("MaxAllowedCurrent", joint_max_current),
+                    ("Temperature", joint_temperature),
+                    ("TemperatureMin", joint_temperature_min),
+                    ("Command", joint_position_actuator.value),
+                    ("Position", joint_position_sensor.value)
+                )
 
-            logger.log(
-                ("Time", timer.dcm_time() / 1000.),
-                ("Current", joint_current),
-                ("CurrentSA", joint_current_sa),
-                ("CurrentLimitHigh", current_limit_high),
-                ("CurrentLimitLow", current_limit_low),
-                ("MaxCurrent", joint_max_current),
-                ("Temperature", joint_temperature),
-                ("TemperatureMax", joint_temperature_min),
-                ("Command", joint_position_actuator.value),
-                ("Position", joint_position_sensor.value)
-            )
+                if plot:
+                    plot_server.add_point("Current", loop_time, joint_current)
+                    plot_server.add_point("CurrentSA", loop_time,
+                                          joint_current_sa)
+                    plot_server.add_point("CurrentLimitHigh", loop_time,
+                                          current_limit_high)
+                    plot_server.add_point("CurrentLimitLow", loop_time,
+                                          current_limit_low)
+                    plot_server.add_point("MaxAllowedCurrent", loop_time,
+                                          joint_max_current)
+                    plot_server.add_point("Temperature", loop_time,
+                                          joint_temperature)
+                    plot_server.add_point("TemperatureMin", loop_time,
+                                          joint_temperature_min)
+
+            except KeyboardInterrupt:
+                flag_loop = False  # out of test loop
+                log.info("KeyboardInterrupt from user")
+
+        log.info("OUT OF TEST LOOP")
 
         result_file_path = "/".join(
             [
@@ -123,6 +170,8 @@ class TestCurrentLimitation:
             [[joint_initial_maximum, dcm.getTime(0)]], "Merge"]
         joint_position_actuator.minimum = [
             [[joint_initial_minimum, dcm.getTime(0)]], "Merge"]
+
+        plot_server.curves_erase()
 
         assert flag_joint
         assert flag_current_limit_low_exceeded
