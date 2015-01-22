dc972c00107a9ebcd4e169f89aaf8e34b6b28e87
david <ddambreville@aldebaran-robotics.com>
2015-01-22 11:13:52 +0100

    [ASUS] add test 3D and compare file scripts
    Change-Id: I00228c5362ed547af95885b7e494dbdce71b4d2d
    Reviewed-on: http://gerrit.aldebaran.lan/52759
    Approved: enalepa <enalepa@aldebaran-robotics.com>
    Reviewed-by: enalepa <enalepa@aldebaran-robotics.com>
    Tested-by: enalepa <enalepa@aldebaran-robotics.com>

--- test_asus/3D_Asus.py
+++ test_asus/3D_Asus.py
@@ -26,14 +26,12 @@ def main():
     parser.add_argument("-v", "--version", action="version",
                         version="%(prog)s 1.0")
 
-    # parser.add_argument("-o", "--output", dest="output"
-    #                   default=DEFAULT_OUTPUT)
-
     args = parser.parse_args()
 
     if not os.path.exists(PATH):
         os.makedirs(PATH)
 
+    min_distance_0 = 0
     MAX_RANGE = 6000
     IP = args.robot_ip
     PORT = 9559
@@ -44,45 +42,30 @@ def main():
         import sys
         sys.exit(0)
 
+    im = None
+    alimage = vd.getImageRemote(name)
+    if(alimage is None):
+        print "Error..."
+        time.sleep(1)
+    width = alimage[0]
+    height = alimage[1]
+    data = alimage[6]
+
+    depth = numpy.array(array.array("H", data)).reshape(height, width)
+    if im is None:
+        depth[0, 0] = MAX_RANGE
+        im = pyplot.imshow(depth, cmap="gist_stern")
+    else:
+        im.set_data(depth)
     try:
-        im = None
         while(True):
-            alimage = vd.getImageRemote(name)
-            if(alimage is None):
-                print "Error..."
-                time.sleep(1)
-                continue
-            width = alimage[0]
-            height = alimage[1]
-            data = alimage[6]
-
-            depth = numpy.array(array.array("H", data)).reshape(height, width)
-            min_distance = 5000
-            for k in range(0, height):
-                for l in range(0, width):
-                    current_value = depth[k][l]
-                    if(current_value < min_distance and current_value != 0):
-                        min_distance = current_value
-
-            if im is None:
-                depth[0, 0] = MAX_RANGE
-                im = pyplot.imshow(depth, cmap="gist_stern")
-            else:
-                im.set_data(depth)
-
             date_log = datetime.datetime.now().strftime("%H-%M-%S_%f")
-            pyplot.savefig("./img/" + date_log + "_" + name + "_dis_" +
-                           str(min_distance), dpi=None, format='eps')
+            pyplot.savefig("./" + PATH + "/" + date_log + "_" + name,
+                           dpi=None, format='eps')
 
     except KeyboardInterrupt:
         vd.unsubscribe(name)
         print "Unsubscribed client " + name + " from ALVideoDevice"
-
-    try:
-        while(True):
-            time.sleep(0.5)
-    except KeyboardInterrupt:
-        pass
 
 if __name__ == '__main__':
     main()
